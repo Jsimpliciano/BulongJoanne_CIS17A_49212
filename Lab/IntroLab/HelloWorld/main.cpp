874383f2eb1bebf9662a5e6b056a9db6d0afda85
Jsimpliciano <Jsimpliciano26@gmail.com>
2020-08-31 23:16:59 -0700

    Signed-off-by: Jsimpliciano <Jsimpliciano26@gmail.com>

---
+++ Lab/IntroLab/HelloWorld/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Joanne Bulong
+ * Created on August 31, 2020, 10:15 PM
+ * Purpose:  "Hello World"
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants - No Global Variables
+//Only Universal Constants, Math, Physics, Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    //Set Random Number seed
+    
+    //Declare Variable Data Types and Constants
+    
+    
+    //Initialize Variables
+    
+    //Process or map Inputs to Outputs
+
+    //Display Outputs
+    cout<<"Hello World"<<endl;
+    
+    //Exit stage right!
+    return 0;
+}
